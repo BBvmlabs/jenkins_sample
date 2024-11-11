37512e52e44576af5ebd7108486508b2f867abdc
BBVMLABS <bbvmlabs@gmail.com>
2024-11-11 19:08:52 +0530

    change on the DB_Task_ cs html file

--- MVCProject/obj/Debug/net8.0/MVCProject.AssemblyInfo.cs
+++ MVCProject/obj/Debug/net8.0/MVCProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa5795841601400d8a1120a432a8b0a55707273")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
